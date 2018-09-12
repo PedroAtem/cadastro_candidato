709d14aa8a9540ca98fbd25b4b284414adea9a6a
Pedro <pedro.phdois@gmail.com>
2018-09-12 15:34:06 -0300

    pagina basica de login criado e sistema basico de rotas criado

--- cadastro-candidato/src/app/app.module.ts
+++ cadastro-candidato/src/app/app.module.ts
@@ -1,14 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule }   from '@angular/router';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './view/login/login.component';
+import { CadastroComponent } from './view/cadastro/cadastro.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    CadastroComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+		{
+			path: 'login',
+			component: LoginComponent
+		},
+		{
+			path: 'cadastro',
+			component: CadastroComponent
+		}
+	])
   ],
   providers: [],
   bootstrap: [AppComponent]
